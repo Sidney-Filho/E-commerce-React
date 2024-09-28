25007db3edc65979e2f2869f7e54c36414f543ef
Sidney Filho <sidinhocostafilho@gmail.com>
2024-09-28 04:04:40 +0100

    adjuste endpoint for products

--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -8,17 +8,20 @@ const router = Router();
 // Rota para obter todos os produtos
 router.get('/', async (req: Request, res: Response) => {
   try {
-    console.log('Request to fetch all products');
     connection.query<RowDataPacket[]>('SELECT * FROM products', (error, results) => {
       if (error) {
-        console.error('Database query error:', error); // Log do erro
         return res.status(500).json({ error: 'Database query error' });
       }
-      console.log('Products fetched:', results); // Log dos produtos obtidos
-      res.json(results);
+
+      // Mapear os resultados para incluir a URL da imagem com base na categoria
+      const productsWithImages = results.map(product => ({
+        ...product,
+        imageUrl: `http://localhost:3001/assets/images/${product.category}/${product.image}` // Inclui a categoria na URL da imagem
+      }));
+
+      res.json(productsWithImages);
     });
   } catch (error) {
-    console.error('Internal server error:', error); // Log do erro
     res.status(500).json({ error: 'Internal server error' });
   }
 });
@@ -37,6 +40,14 @@ router.get('/:id', async (req: Request, res: Response) => {
         console.log('Product not found for ID:', id); // Log se o produto não for encontrado
         return res.status(404).json({ error: 'Product not found' });
       }
+
+      // Adiciona a URL da imagem ao produto retornado
+      const productWithImage = {
+        ...results[0],
+        imageUrl: `http://localhost:3001/assets/images/${results[0].category}/${results[0].image}`
+      };
+
+      res.json(productWithImage);
     });
   } catch (error) {
     console.error('Internal server error:', error); // Log do erro
